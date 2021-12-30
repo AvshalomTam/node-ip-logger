7b39f823d907f73711dc3e4ebbedaa6d11f81e9f
Avshalom <avshalom.tam@gmail.com>
2021-12-30 14:29:45 +0000

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 443
 const publicDirectoryPath = path.join(__dirname, './public')
 
 // import from utils
